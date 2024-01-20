e121bf5a6a1020f1d176ac5d0d7e2ee56629bf45
younes1377 <118458466+younes1377@users.noreply.github.com>
2024-01-20 18:42:03 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 // @armanibash
 import { connect } from 'cloudflare:sockets';
 
-let userID = '3f0fa7b8-36ee-449e-b4a2-5130dcd42461';
+let userID = '46fffef6-b8fe-40e3-bc9b-6a8b16a9e0b6';
 
 const proxyIPs = ["[2001:67c:2b0:db32:0:1:681a:404]"]; // ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
